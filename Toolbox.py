46ba861458d16a0c68e05c50f3cb3ab92a049e54
DanielRJohansen <drdj@hotmail.dk>
2020-06-16 13:42:24 +0200

    Create some toolbox functions

---
+++ Toolbox.py
@@ -0,0 +1,32 @@
+from glob import glob
+import os
+import torch
+
+def get_subs(folder):
+    return glob(folder + r"\*")
+
+def min_images(folder):
+
+    fewest = 9999
+    bins = [0]*1000
+    for sf in get_subs(folder):
+        num_images = len(get_subs(sf))
+        bins[num_images] += 1
+        if num_images < fewest:
+            fewest = num_images
+            print(sf, fewest)
+
+    print("Fewest images in all folders:", fewest)
+    print(bins)
+
+def delete_files(folder, f_format=".csv", suffix_len=4):
+    print("Removing {} files from {}".format(f_format, folder))
+    for sf in get_subs(folder):
+        for file in get_subs(sf):
+            if file[-suffix_len:] == f_format:
+                os.remove(file)
+
+
+def save_tensor(dst_folder):
+    tt = torch.tensor((0,1), dtype=torch.float32)
+    torch.save(tt, dst_folder + r"\file" + ".pt")
---
+++ main.py
@@ -0,0 +1,10 @@
+import Toolbox
+
+
+source_folder = r"D:\CTdeeper\NIH_scans\Images_png"
+work_folder = r"E:\DLimages\15_25_60"
+
+
+#Toolbox.delete_files(source_folder)
+#Toolbox.min_images(source_folder)
+Toolbox.save_tensor(work_folder)
