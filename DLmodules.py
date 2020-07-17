Fix error (softmax) in zTop, to sigmoid

DLmodules.py
@@ -28,7 +28,7 @@ class BlockB(nn.Module):
 
         self.bn = nn.BatchNorm3d(64)
 
-        print("Block weights:", sum(p.numel() for p in self.parameters() if p.requires_grad))
+        #print("Block weights:", sum(p.numel() for p in self.parameters() if p.requires_grad))
 
 
     def forward(self, batch):
@@ -130,11 +130,11 @@ class DLCNN(nn.Module):
         #batch = torch.cat((branch_1, batch), 1)
         return batch
 
-    def loadModel(self):
-        self.load_state_dict(torch.load(Constants.model_path))
+    def loadModel(self, model_path):
+        self.load_state_dict(torch.load(model_path))
 
     def saveModel(self):
-        torch.save(self.state_dict(), Constants.model_path)
+        torch.save(self.state_dict(), r"./baseonly_model.pt")
 
 
 
@@ -145,7 +145,7 @@ class zTop(nn.Module):
         self.flat = nn.Conv3d(16, 16, kernel_size=3, stride=1, padding=(0,1,1))
         self.bn = nn.BatchNorm3d(16)      # Only works with batch_size > 1
         self.fc = nn.Linear(16*1*32*32, 1)
-        self.sm = nn.Softmax(1)
+        self.af = nn.Sigmoid()
 
         self.weigths = sum(p.numel() for p in self.parameters() if p.requires_grad)
         print("zTop loaded. Weights: ", f"{self.weigths:,}")
@@ -156,7 +156,7 @@ class zTop(nn.Module):
         batch = self.bn(batch)
         batch = batch.view(batch.size()[0], -1)
         batch = self.fc(batch)
-        batch = self.sm(batch)
+        batch = self.af(batch)
         return batch
 
 

DumbLesion.py
@@ -61,8 +61,8 @@ class DumbLesionNet(nn.Module):
         batch = self.top.forward(batch)
         return batch
 
-    def _train(self):
-        best_acc = -999999
+    def _train(self, best_acc):
+
 
         for i in range(self.epochs):
             self.train()  # Notify PyTorch entering training mode.
@@ -74,7 +74,7 @@ class DumbLesionNet(nn.Module):
                 self.optimizer.zero_grad()
 
                 input, hist, label = self.batcher.getBatch()
-                #input.require_grad = True
+                input.require_grad = True
                 prediction = self.__forward(input)
 
                 loss, acc = self.loss(prediction, label.to(self.device), self.device)
@@ -90,6 +90,8 @@ class DumbLesionNet(nn.Module):
             if self.val_acc_history[-1] > best_acc:
                 best_acc = self.val_acc_history[-1]
                 self.base.saveModel()
+                print()
+                print("Saving current model")
 
             self.acc_history.append(np.mean(ep_acc))
             self.loss_history.append(np.mean(ep_loss))
@@ -123,8 +125,6 @@ class DumbLesionNet(nn.Module):
 
     def plotStuffs(self, best_acc):
         fig, axs = plt.subplots(2)
-        #plt.plot(self.loss_history, 'b', label="Loss")
-        #plt.legend()
 
         axs[0].plot(self.acc_history, label="Train acc.")
         axs[0].plot(self.val_acc_history, label="Val Acc.")
@@ -136,20 +136,10 @@ class DumbLesionNet(nn.Module):
         plt.xlabel("Epoch")
         plt.ylabel("Loss")
         axs[1].legend()
-        #ax2 = ax.twinx()
-        #plt.plot(self.acc_history, 'r', label="Train acc.")
-        #plt.legend()
-
-        #p2 = plt.plot(self.val_acc_history, label="Val Acc.")
-        #plt.legend()
 
         plt.savefig('./Plots/{:.4f}_plot.png'.format(best_acc))
         plt.show()
 
-        #fig, (ax1, ax2) = plt.subplots(1, 2)
-        #fig.suptitle('Horizontally stacked subplots')
-        #.plot(x, y)
-        #ax2.plot(x, -y)
 
     def trainMode(self):
         self.train()

LossCalculator.py
@@ -33,7 +33,9 @@ class OrderLoss:
 
 
 def zLoss(predictions, labels, device):
-    return torch.mean(torch.abs(torch.sub(torch.flatten(predictions), labels)))
+    loss = torch.mean(torch.abs(torch.sub(torch.flatten(predictions), labels)))
+    acc = 1-loss.item()
+    return loss, acc
 
 
 def IoULoss(prediction, label, device):

Toolbox.py
@@ -146,8 +146,15 @@ def makeAOCLabels(dst, data_file, num_areas):
                 name = os.path.join(dst, sf, center[:-4])
                 torch.save(label, name + "_AOCLabel.pt")
 
-def cleanElement( string):
-    return string[:3]
+
+def cleanElement(string):
+    index = 0
+    while index < len(string):
+        if string[index] == '_':
+            break
+        index += 1
+    return string[:index]
+
 def elementExists(f, sf, name):
     if not os.path.isdir(os.path.join(f, sf)):
         return False
@@ -172,7 +179,7 @@ def makeZLabels(dst, data_file):
             name = center[:-4]
             path = os.path.join(dst, sf, name)
             if elementExists(dst, sf, name):
-                z = torch.tensor(toList(row[8])[2])
+                z = torch.tensor(toList(row[8])[2], dtype=torch.float32)
                 torch.save(z, path + "_zlabel.pt")
 
 def visualizeLabel(im_path, label_path):